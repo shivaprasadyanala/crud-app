e7e001d0a87b0d0c6c32f57f715485ef87dfffd7
shivaprasadyanala <shivaprasadyanala@gmail.com>
2021-12-03 07:06:58 +0530

    added nav-bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import Create from './components/create';
 import Read from './components/read';
 import Update from './components/update';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 function App() {
   return (
     // <Create />
     <Router>
       <div className="main">
         <h1 className="main-header text-red-400 font-mono text-center">React Crud Operations</h1>
+        <div className="grid justify-items-center">
+          <nav className="bg-gray-600 text-white w-96 p-3">
+            <ul className="flex flex-row place-content-around">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/create">create tasks</Link>
+              </li>
+              <li>
+                <Link to="/read">read tasks</Link>
+              </li>
+
+            </ul>
+          </nav>
+
+        </div>
         <div>
           <Routes>
             <Route path="/create" element={<Create />} />
